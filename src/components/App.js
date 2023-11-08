19e9ead809b56b0bed3205968d6bcf7c3c0abafd
Diana Chepkoech <dianachepkoech336@gmail.com>
2023-11-08 04:22:52 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
-// create your App component here
+
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching dog image:', error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Random Black Dog Image</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random  Black Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
